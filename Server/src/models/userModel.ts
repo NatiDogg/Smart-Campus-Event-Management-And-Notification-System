3538210824bf5e6e7cfc96b7c2bc7ca8232a4859
Nati Dogg <natnael.wondmu@gmail.com>
2026-04-20 06:31:01 -0700

    generative ai package got installed

--- Server/src/models/userModel.ts
+++ Server/src/models/userModel.ts
@@ -50,7 +50,45 @@ const userSchema = new mongoose.Schema({
 
 },{timestamps: true, discriminatorKey: "role"});
 
+
+userSchema.pre('findOneAndDelete', async function () {
+ 
+  const docToUpdate = await this.model.findOne(this.getQuery());
+  
+  if (docToUpdate) {
+    const userId = docToUpdate._id;
+    if (docToUpdate.role === 'organizer') {
+      // Find all events created by this organizer
+      const organizerEvents = await mongoose.model('event').find({ organizedBy: userId }).select('_id');
+      
+      // Extract just the IDs into an array: [id1, id2, id3...]
+      const eventIds = organizerEvents.map(event => event._id);
+
+      if (eventIds.length > 0) {
+        // Delete all registrations for all events owned by this organizer
+        await mongoose.model('registration').deleteMany({ eventId: { $in: eventIds } });
+        
+        // Delete all feedback for all events owned by this organizer
+        await mongoose.model('feedback').deleteMany({ eventId: { $in: eventIds } });
+        
+        // Finally, delete the events themselves
+        await mongoose.model('event').deleteMany({ organizedBy: userId });
+        
+        
+      }
+    }
+    // Cleaning up related collections
+    await mongoose.model('registration').deleteMany({ studentId: userId });
+    await mongoose.model('feedback').deleteMany({ studentId: userId });
+    await mongoose.model('subscription').deleteMany({studentId: userId});
+    
+    
+  }
+});
+
 const userModel = mongoose.models.user || mongoose.model("user",userSchema);
+
+
 
 export default userModel;
 
