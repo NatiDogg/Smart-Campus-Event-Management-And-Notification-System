ab6996963358b6ca7d71f8e60df2a91c5c00c058
Nati Dogg <natnael.wondmu@gmail.com>
2026-05-02 08:06:50 -0700

    pdf generater library installed

--- Client/src/pages/student/Events.jsx
+++ Client/src/pages/student/Events.jsx
@@ -137,13 +137,13 @@ const Events = () => {
                  <path strokeLinecap="round" strokeLinejoin="round" d="M21 21l-5.197-5.197m0 0A7.5 7.5 0 105.196 5.196a7.5 7.5 0 0010.607 10.607z" />
               </svg>
            </div>
-          <p className="text-gray-400 font-bold capitalize text-sm ">No matching events found</p>
-          <button 
+          <p className="text-gray-400 font-bold capitalize text-sm ">{searchQuery ? 'No matching events found' : 'No events created yet'}</p>
+         {searchQuery && <button 
             onClick={() => {setSearchQuery(''); setSelectedCategory('All'); setSelectedOrganizer('');}}
             className="mt-4 text-blue-500 text-sm font-semibold hover:underline cursor-pointer"
           >
             Clear all filters
-          </button>
+          </button>}
           
         </div>
       ) : (
